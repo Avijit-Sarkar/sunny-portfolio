d1393cf59ec3513f3eb1d1398ee3657649629278
Avijit <avijitsarkar2222@gmail.com>
2023-04-12 20:12:45 +0530

    creating the experience card

--- components/About.tsx
+++ components/About.tsx
@@ -9,7 +9,7 @@ function About({}: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl justify-evenly mx-auto scroll-smooth items-center"
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl justify-evenly mx-auto items-center"
     >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-cyan-700/70 text-2xl">
         About
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 type Props = {};
 
 function Experience({}: Props) {
   return (
-    <div className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center">
+    <motion.div className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-cyan-700/70 text-2xl">
         Experience
       </h3>
-    </div>
+      <div></div>
+    </motion.div>
   );
 }
 
